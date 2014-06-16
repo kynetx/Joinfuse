e22e8f92871ad846ab2258ae3d03672081cc9a02
windley <pjw@pjw.byu.edu>
2014-06-16 11:19:13 -0600

    Login server undefined

--- js/CloudOS-config.js
+++ js/CloudOS-config.js
@@ -3,3 +3,4 @@ CloudOS.appKey = "D98022C6-C4F4-11E3-942D-E857D61CF0AC";
 CloudOS.anonECI = "85255500-0b65-0130-243c-00163ebcdddd";
 CloudOS.callbackURL = "http://windley.github.io/Joinfuse/code.html";
 CloudOS.host = "kibdev.kobj.net";
+CloudOS.login_server = "kibdev.kobj.net";

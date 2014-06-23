aab3a00f1c9fe6f338f249af80b7eecb33e0e1ce
windley <pjw@pjw.byu.edu>
2014-06-23 11:40:21 -0600

    More wording

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 
     var router=new $.mobile.Router( [
        {"#page-authorize": {handler: "pageAuthorize",
-				 events: "csh", // just do when we create the page
+				 events: "s", // just do when we create the page
 				 argsre: true
 				} },
        {"#page-manage-fuse": {handler: "pageManageFuse",

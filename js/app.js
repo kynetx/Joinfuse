fc5df86cf4f64de711f47a9ce53fa92621cf8042
windley <pjw@pjw.byu.edu>
2014-06-23 11:50:29 -0600

    Stupid loader

--- js/app.js
+++ js/app.js
@@ -351,7 +351,8 @@
     // pull the session out of the cookie.
     $(document).bind("mobileinit", onMobileInit);
     $(document).ready(onPageLoad);
-
+    console.log("Loaded page");
+	    $.mobile.loading("hide");
 
 
 })(jQuery);

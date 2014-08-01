ed01cf77c95a4bb95b75d9485e3a4643b40591e3
Phil Windley <github@windley.org>
2014-08-01 13:54:11 -0600

    Update CloudOS-config.js

--- js/CloudOS-config.js
+++ js/CloudOS-config.js
@@ -2,5 +2,5 @@
 CloudOS.appKey = "682607CE-FB1F-11E3-ACCF-7766410AC9C1";
 CloudOS.anonECI = "85255500-0b65-0130-243c-00163ebcdddd";
 CloudOS.callbackURL = "http://joinfuse.com/code.html";
-CloudOS.host = "kibdev.kobj.net";
-CloudOS.login_server = "kibdev.kobj.net";
+CloudOS.host = "cs.kobj.net";
+CloudOS.login_server = "cs.kobj.net";

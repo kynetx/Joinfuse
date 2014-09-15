c9aba6718f856ce60f61f727f41971655e65a065
Phil Windley <phil@windley.org>
2014-09-15 09:04:52 -0600

    move vehicle paining inside Vehicle.summary() call

--- js/app.js
+++ js/app.js
@@ -173,23 +173,23 @@
 					}));
 			    }
 			}
-		    });
 
-		    function sortBy(prop){
-			return function(a,b){
-			    if( a[prop] < b[prop]){
-				return 1;
-			    }else if( a[prop] > b[prop] ){
-				return -1;
-			    }
-			    return 0;
+			function sortBy(prop){
+			    return function(a,b){
+				if( a[prop] < b[prop]){
+				    return 1;
+				}else if( a[prop] > b[prop] ){
+				    return -1;
+				}
+				return 0;
+			    };
 			};
-		    };
 
-		    var keys = json.sort(sortBy("profileName"));
-		    $.each(keys, paint_item);
+			var keys = json.sort(sortBy("profileName"));
+			$.each(keys, paint_item);
 
-		    $('#manage-fleet').listview('refresh');
+			$('#manage-fleet').listview('refresh');
+		    });
 		});
 	    });
 	}, 

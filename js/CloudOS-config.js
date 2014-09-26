82d9f72e08a581950d29ab89813b244b8d1636e6
Phil Windley <github@windley.org>
2014-09-26 11:45:44 -0500

    Create CloudOS-config.js
    Doh! We can't delete this.

---
+++ js/CloudOS-config.js
@@ -0,0 +1,6 @@
+-// do we need this? Oauth? What's that? Not in the mobile app...
+-CloudOS.appKey = "682607CE-FB1F-11E3-ACCF-7766410AC9C1";
+-CloudOS.anonECI = "85255500-0b65-0130-243c-00163ebcdddd";
+-CloudOS.callbackURL = "http://joinfuse.com/code.html";
+-CloudOS.host = "cs.kobj.net";
+-CloudOS.login_server = "kibdev.kobj.net";
